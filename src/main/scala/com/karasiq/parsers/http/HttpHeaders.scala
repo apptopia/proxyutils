07ac786145c722b5a23af97f50d5ee545c871356
Alex Zolotko <azolotko@gmail.com>
2018-10-26 12:40:16 +0200

    Proxy server should establish a direct connection with upstream HTTP proxy

--- src/main/scala/com/karasiq/parsers/http/HttpHeaders.scala
+++ src/main/scala/com/karasiq/parsers/http/HttpHeaders.scala
@@ -4,17 +4,20 @@ import akka.util.ByteString
 import com.karasiq.networkutils.http.headers.HttpHeader
 
 private[http] object HttpHeaders {
-  private def asByteString(b: Seq[Byte]) = ByteString(b:_*)
+  private def asByteString(b: Seq[Byte]) = ByteString(b: _*)
 
   private def headersEnd: String = "\r\n\r\n"
 
   def unapply(b: Seq[Byte]): Option[(Seq[HttpHeader], Seq[Byte])] = {
-    val regex = """(?s)(?:\r\n)?([\w-]+): ((?:(?!\r\n[\w-]+: )(?!\r\n\r\n).)+)""".r
+    val regex =
+      """(?s)(?:\r\n)?([\w-]+): ((?:(?!\r\n[\w-]+: )(?!\r\n\r\n).)+)""".r
     asByteString(b).utf8String.split(headersEnd, 2).toSeq match {
       case h +: rest ⇒
         val headers = regex.findAllMatchIn(h).collect {
           case regex(name, value) ⇒
-            HttpHeader(name, value.lines.map(_.dropWhile(_ == ' ')).mkString("\r\n"))
+            HttpHeader(
+              name,
+              value.lines.map(_.dropWhile(_ == ' ')).toArray.mkString("\r\n"))
         }
         Some(headers.toVector → ByteString(rest.mkString))
 
